962c44325e259acdab7b238ebc2769846297e9fc
pldrobot <pldrobots@gmail.com>
2021-12-15 01:39:49 +0530

    'update'

--- Wave_App/app.py
+++ Wave_App/app.py
@@ -1,5 +1,10 @@
 from h2o_wave import Q, ui, app, main, data, pack
 import folium
+from fsplit.filesplit import Filesplit
+import pickle
+import numpy as np
+import datetime
+import os
 # import io
 # import base64
 # from matplotlib import colors
@@ -59,6 +64,69 @@ def deletePages(q):
     for page in pages:
         del q.page[page]
 
+def cal2jd(date):
+    fmt = '%Y-%m-%d'
+    sdtdate = datetime.datetime.strptime(date, fmt)
+    return sdtdate.toordinal() + 1721424.5
+
+
+def showSeverity(lat, lon, date):
+    if(not os.path.isfile('Appdata/W_Data_Dict.pickle')):
+        fs = Filesplit()
+
+        def merge_cb(f, s):
+            print("file: {0}, size: {1}".format(f, s))
+        fs.merge(input_dir="Appdata", callback=merge_cb)
+
+    else:
+        print('Data File Available')
+    open_file_ = open("Appdata/W_Data_Dict.pickle", "rb")
+    W_Data_Dict = pickle.load(open_file_)
+    open_file_.close()
+
+    # lons = np.linspace(-168.87, -65.25694444, num=600)
+    # lats = np.linspace(17.93972222, 70.3306, num=300)
+    lons = []
+    lats = []
+    keys = list(W_Data_Dict)
+    for key in keys:
+        temp = key.split(',')
+        # lons.append(float(temp[0]))
+        lats.append(float(temp[1]))
+    lats = np.sort(np.array(lats))
+    lat_cat = np.searchsorted(lats, lat)
+    lat_cat = round(lats[lat_cat], 4)
+
+    for key in keys:
+        temp = key.split(',')
+        if(float(temp[1]) == lat_cat):
+            lons.append(float(temp[0]))
+
+    lons = np.sort(np.array(lons))
+    lon_cat = np.searchsorted(lons, lon)
+    lon_cat = round(lons[lon_cat], 4)
+
+    location = str('%.4f' % lon_cat)+','+str('%.4f' % lat_cat)
+    print(location)
+    w_data_year = W_Data_Dict[location]
+    doy = datetime.datetime.strptime(date, '%Y-%m-%d').timetuple().tm_yday
+
+    w_data_dict = {}
+    header = ['YEAR', 'MO', 'DY', 'T2M', 'T2MDEW', 'T2MWET', 'TS', 'T2M_RANGE', 'T2M_MAX', 'T2M_MIN', 'QV2M', 'RH2M',
+              'PRECTOTCORR', 'PS', 'WS10M', 'WS10M_MAX', 'WS10M_MIN', 'WS10M_RANGE', 'WS50M', 'WS50M_MAX', 'WS50M_MIN']
+    for i in range(0, 7):
+        w_data = w_data_year[doy-6+i]
+        for ind, val in enumerate(header[3:], start=3):
+            key = val+'_'+str(i)
+            w_data_dict[key] = float(w_data[ind])
+    entries_to_remove = ('T2M_RANGE_0', 'QV2M_0', 'QV2M_1', 'T2M_RANGE_2', 'QV2M_2',
+                         'QV2M_3', 'T2M_RANGE_4', 'T2M_RANGE_5', 'PRECTOTCORR_5', 'T2M_RANGE_6')
+    for k in entries_to_remove:
+        w_data_dict.pop(k, None)
+    final_list = [lon, lat, cal2jd(date)]+list(w_data_dict.values())
+
+    return final_list
+    
 async def loadPage(q):
     q.page['inputLatLong'] = ui.form_card(
         box='1 2 10 4',
